56d9082b04cdc393ece8d2d2a5b1c04fc9737744
A Dev <82761748+ADev531@users.noreply.github.com>
2022-05-07 20:23:39 +0900

    Update adsystem.js

--- adsystem.js
+++ adsystem.js
@@ -2,8 +2,9 @@ const body = document.getElementById("body");
 const AD = createAD();
 
 function createAD() {
-  const ad = document.createElement("img");
-  body.appendChild(ad);
+  const tmp = document.createElement("img");
+  tmp.src = "./ad/testad.png";
+  body.appendChild(tmp);
   
-  return ad
+  return tmp
 }
